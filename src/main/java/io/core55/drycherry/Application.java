833bf1475b26ff6d0b6dba22ff9aac186e8a7616
Simone Stefani <s.stefani95@gmail.com>
2017-04-20 15:10:00 +0200

    Add application main method

---
+++ src/main/java/io/core55/drycherry/Application.java
@@ -0,0 +1,15 @@
+package io.core55.drycherry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by S. Stefani on 2017-04-20.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

571dcee99e2b7eeff47de83dfe35a08e5be65b9a
Simone Stefani <s.stefani95@gmail.com>
2017-04-22 20:42:26 +0200

    Add API versioning

--- src/main/java/io/github/core55/core/BaseEntity.java
+++ src/main/java/io/github/core55/core/BaseEntity.java
@@ -6,16 +6,16 @@
 
 package io.github.core55.core;
 
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;

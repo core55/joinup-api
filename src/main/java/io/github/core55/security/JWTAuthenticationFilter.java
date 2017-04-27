a5fb99786e705cc1b7ddda007387d628ed9b28e7
gajland <gajland@kth.se>
2017-04-27 09:48:08 +0200

    Add JWTAuthenticationFilter

---
+++ src/main/java/io/github/core55/security/JWTAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package io.github.core55.security;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by P. Gajland on 2017-04-27.
+ */
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        Authentication authentication = TokenAuthenticationService
+                .getAuthentication((HttpServletRequest) request);
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}

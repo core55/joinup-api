1fa15e77ed9dd8b9c65bf0330ddf5f08d68d847c
gajland <gajland@kth.se>
2017-05-12 09:50:59 +0200

    Allow POST requests to ~/send

--- src/main/java/io/github/core55/config/WebSecurityConfig.java
+++ src/main/java/io/github/core55/config/WebSecurityConfig.java
@@ -48,6 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
 //                .antMatchers("/api/login/**").permitAll()
                 .antMatchers("/api/**").permitAll() // Temporary
+                .antMatchers("/send").permitAll() // Temporary
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JWTAuthenticationFilter(),

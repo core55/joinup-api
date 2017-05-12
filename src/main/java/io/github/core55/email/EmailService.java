411fa8b3d020a614515be5c0a11951fdea5ef422
gajland <gajland@kth.se>
2017-05-12 09:51:57 +0200

    Add SendGrid infrastructure

--- src/main/java/io/github/core55/email/EmailService.java
+++ src/main/java/io/github/core55/email/EmailService.java
@@ -1,12 +1,12 @@
 package io.github.core55.email;
 
-import org.springframework.mail.MailException;
+import com.sendgrid.*;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import java.io.IOException;
 
 @Service
 @EnableAutoConfiguration
@@ -22,18 +22,24 @@ public class EmailService {
     }
 
     public void prepareAndSend(String recipient, String subject, String link) {
-        MimeMessagePreparator messagePreparator = mimeMessage -> {
-            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-            messageHelper.setFrom("hello@culater.com");
-            messageHelper.setTo(recipient);
-            messageHelper.setSubject(subject);
-            String content = mailContentBuilder.build(link);
-            messageHelper.setText(content, true);
-        };
+        Email from = new Email("app67253195@heroku.com");
+        Email to = new Email(recipient);
+        Content content = new Content("text/plain", "Hello, Email! \n Here is your login link: " + link);
+        Mail mail = new Mail(from, subject, to, content);
+
+        // TODO: Add SENDGRID_API_KEY
+        SendGrid sg = new SendGrid(" ");
+        Request request = new Request();
         try {
-            javaMailSender.send(messagePreparator);
-        } catch (MailException e) {
-            // Runtime exception
+            request.method = Method.POST;
+            request.endpoint = "mail/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            System.out.println(response.statusCode);
+            System.out.println(response.body);
+            System.out.println(response.headers);
+        } catch (IOException ex) {
+
         }
     }
 }
